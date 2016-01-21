38821d15ad0311679460e8f033ece881e120d98d
Syfaro <syfaro@foxpaw.in>
2016-01-20 22:18:34 -0600

    Add better formatting for listing users who selected an option

--- commands/manage/selection_list.go
+++ commands/manage/selection_list.go
@@ -5,6 +5,7 @@ import (
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/syfaro/finch"
 	"github.com/syfaro/selectionsbot/database"
+	"strings"
 )
 
 func init() {
@@ -35,21 +36,14 @@ func (cmd selectionList) Execute(update tgbotapi.Update) error {
 		return err
 	}
 
-	var list []database.SelectionList
-	err = database.DB.Select(&list, `
+	var items []database.SelectionItem
+	err = database.DB.Select(&items, `
 		select
-			user.name as user,
-			selection_item.item as item
+			*
 		from
-			selection_vote
-		inner join
-			user on
-				selection_vote.user_id = user.id
-		inner join
-			selection_item on
-				selection_item.id = selection_vote.selection_item_id
+			selection_item
 		where
-			selection_vote.selection_id = $1
+			selection_id = $1
 	`, selection.ID)
 	if err != nil {
 		return err
@@ -57,10 +51,47 @@ func (cmd selectionList) Execute(update tgbotapi.Update) error {
 
 	b := bytes.Buffer{}
 
-	for _, item := range list {
-		b.WriteString(item.User)
-		b.WriteString(" - ")
+	for _, item := range items {
+		var votes []database.SelectionVote
+		err = database.DB.Select(&votes, `
+			select
+				*
+			from
+				selection_vote
+			where
+				selection_id = $1 and
+				selection_item_id = $2
+		`, selection.ID, item.ID)
+		if err != nil {
+			return err
+		}
+
+		var users []string
+
+		for _, vote := range votes {
+			var user database.User
+			err = database.DB.Get(&user, `
+				select
+					*
+				from
+					user
+				where
+					id = $1
+			`, vote.UserID)
+			if err != nil {
+				return err
+			}
+
+			users = append(users, user.Name)
+		}
+
 		b.WriteString(item.Item)
+		b.WriteString(" - ")
+		if len(users) == 0 {
+			b.WriteString("None")
+		} else {
+			b.WriteString(strings.Join(users, ", "))
+		}
 		b.WriteString("\n")
 	}
 
